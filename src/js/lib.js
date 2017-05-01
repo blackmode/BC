f5ed7c55772b350829b2641f9f6c5d6d7f054211
Tomáš Slunsky <t.slunsky@gmail.com>
2017-05-01 15:28:58 +0200

    update

--- src/js/lib.js
+++ src/js/lib.js
@@ -540,18 +540,15 @@ function checkIfFileExists(url)
         return false;
     }
 }
+
+
 /*
-
-HAVE_NOTHING		0	No information is available about the media resource.
-HAVE_METADATA		1	Enough of the media resource has been retrieved that the metadata attributes are initialized. Seeking will no longer raise an exception.
-HAVE_CURRENT_DATA	2	Data is available for the current playback position, but not enough to actually play more than one frame.
-HAVE_FUTURE_DATA	3	Data for the current playback position as well as for at least a little bit of time into the future is available (in other words, at least two frames of video, for example).
-HAVE_ENOUGH_DATA	4	Enough data is available—and the download rate is high enough—that the media can be played through to the end without interruption.
-
-
-
+	HAVE_NOTHING		0	No information is available about the media resource.
+	HAVE_METADATA		1	Enough of the media resource has been retrieved that the metadata attributes are initialized. Seeking will no longer raise an exception.
+	HAVE_CURRENT_DATA	2	Data is available for the current playback position, but not enough to actually play more than one frame.
+	HAVE_FUTURE_DATA	3	Data for the current playback position as well as for at least a little bit of time into the future is available (in other words, at least two frames of video, for example).
+	HAVE_ENOUGH_DATA	4	Enough data is available—and the download rate is high enough—that the media can be played through to the end without interruption.
 */
-
 function videoReady(video) {
 	if ( video.readyState >= video.HAVE_CURRENT_DATA ) 
 	{
@@ -559,7 +556,6 @@ function videoReady(video) {
 	}
 	return false;
 }
-
 
 
 
@@ -608,7 +604,7 @@ function createVideo( src ) {
     video.autoplay = false;
     video.src = src;
     video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
-	video.load();
+	//video.load();
 	return video;
 }
 
